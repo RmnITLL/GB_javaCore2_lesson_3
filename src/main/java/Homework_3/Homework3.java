3c72e35e3dac296a982157c256882a531950efc4
RmnITLL <inetmilo@rambler.ru>
2022-04-27 08:51:35 +0300

    add in new file Hello World!

--- src/main/java/Homework_3/Homework3.java
+++ src/main/java/Homework_3/Homework3.java
@@ -3,5 +3,6 @@ package Homework_3;
 public class Homework3 {
     public static void main(String[] args) {
         System.out.println("Hello World, and World!");
+        System.out.println();
     }
 }
